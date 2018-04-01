69ff2f4894d57df78b3404a436bc1d460e15e45b
nikotyan <nekotanik@gmail.com>
2018-04-01 19:21:33 +0300

    modal contact us

---
+++ js/script.js
@@ -0,0 +1,39 @@
+  var link = document.querySelector(".write-us-button");
+  var popup = document.querySelector(".modal-contact-us");
+  var close = popup.querySelector(".modal-close");
+  var name_surname = popup.querySelector("[name=name]");
+  var email = popup.querySelector("[name=email]");
+  var form = popup.querySelector("form");
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    name.focus;
+  });
+
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!name_surname.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    }
+
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
